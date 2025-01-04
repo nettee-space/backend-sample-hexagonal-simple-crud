c1afd26449ab1dc85b3d5301108a6aea6c9341af
JeongJeongYong <wjddyd94@naver.com>
2025-01-04 19:08:05 +0900

    feat(board): add query and command ports for board domain operations

---
+++ src/main/java/me/nettee/board/application/port/BoardCommandPort.java
@@ -0,0 +1,14 @@
+package me.nettee.board.application.port;
+
+
+import me.nettee.board.application.domain.Board;
+
+public interface BoardCommandPort {
+
+    Board create(Board board);
+
+    Board update(Board board);
+
+    void delete(Long id);
+
+}
