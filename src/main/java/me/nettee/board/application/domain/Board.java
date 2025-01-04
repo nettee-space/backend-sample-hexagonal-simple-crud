c3e02a61f371a95b5950a670f76088e04dcf68db
JeongJeongYong <wjddyd94@naver.com>
2025-01-04 19:04:15 +0900

    feat(board): add prototype of `Board` domain

---
+++ src/main/java/me/nettee/board/application/domain/Board.java
@@ -0,0 +1,34 @@
+package me.nettee.board.application.domain;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Board {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    // TODO: 요구 사항에 따라 필드가 추가될 수 있습니다.
+
+    private Instant deletedAt;
+
+    @Builder
+    public Board(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.deletedAt = null;
+    }
+
+}
