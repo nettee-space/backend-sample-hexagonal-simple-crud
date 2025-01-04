37e68b2599336512dc1894ef430c5f9b7f33365b
jingyu <wlsrb97@naver.com>
2025-01-05 02:06:19 +0900

    chore: Replace spring-boot-starter with spring-boot-starter-web and data-jpa

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     // logging
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
