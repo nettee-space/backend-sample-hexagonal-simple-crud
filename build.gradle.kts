3452db49f0987fb942c04f0f250198d6d0649f60
JeongJeongYong <wjddyd94@naver.com>
2025-01-05 12:15:55 +0900

    style: remove blank line

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,6 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.mockk:mockk:1.13.12")
     testImplementation(kotlin("script-runtime"))
-
 }
 
 kotlin{
