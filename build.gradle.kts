b5c2315911cabb9e65276577dafc41ed28c82b56
silberbullet <pkw961028@naver.com>
2025-01-07 17:05:53 +0900

    build(board):add mapstruct libary

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,6 +50,11 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
+    // mapstruct
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
