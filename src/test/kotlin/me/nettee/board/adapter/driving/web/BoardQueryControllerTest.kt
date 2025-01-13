85f451963d34ff382c41f2f5a8a902dd9f77bce4
jingyu <wlsrb97@naver.com>
2025-01-13 23:22:11 +0900

    refactor: refactor BoardQueryControllerTest

--- src/test/kotlin/me/nettee/board/adapter/driving/web/BoardQueryControllerTest.kt
+++ src/test/kotlin/me/nettee/board/adapter/driving/web/BoardQueryControllerTest.kt
@@ -11,6 +11,7 @@ import me.nettee.board.adapter.driving.web.mapper.BoardDtoMapper
 import me.nettee.board.application.domain.Board
 import me.nettee.board.application.domain.type.BoardStatus
 import me.nettee.board.application.usecase.BoardReadUseCase
+import org.apache.coyote.BadRequestException
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.anyLong
@@ -22,11 +23,14 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActionsDsl
 import org.springframework.test.web.servlet.get
+import org.springframework.web.server.ResponseStatusException
 import java.time.Instant
 
 
@@ -41,37 +45,20 @@ class BoardQueryControllerTest(
 
     val objectMapper = ObjectMapper()
 
-    lateinit var board1: Board
-    lateinit var board2: Board
-    lateinit var board3: Board
-    lateinit var board4: Board
-    lateinit var board5: Board
-    lateinit var board6: Board
-    lateinit var board7: Board
     lateinit var boardList: List<Board>
-    lateinit var pageable: Pageable
-    lateinit var boardPage: Page<Board>
 
     beforeSpec {
         objectMapper.registerModule(JavaTimeModule())
         objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE)
 
-        board1 = Board(1,"title1","content1", BoardStatus.ACTIVE, Instant.now(), null, null)
-        board2 = Board(2,"title2","content2", BoardStatus.ACTIVE, Instant.now(), null, null)
-        board3 = Board(3,"title3","content3", BoardStatus.ACTIVE, Instant.now(), null, null)
-        board4 = Board(4,"title4","content4", BoardStatus.ACTIVE, Instant.now(), null, null)
-        board5 = Board(5,"title5","content5", BoardStatus.ACTIVE, Instant.now(), null, null)
-        board6 = Board(6,"title6","content6", BoardStatus.ACTIVE, Instant.now(), null, null)
-        board7 = Board(7,"title7","content7", BoardStatus.ACTIVE, Instant.now(), null, null)
-
-        boardList = listOf(board1, board2, board3, board4, board5, board6, board7)
-        pageable = PageRequest.of(0, 10)
-        boardPage = PageImpl(boardList, pageable, boardList.size.toLong())
-
+        // list 반복문 완성 코드 ***
+        boardList = (1..14).map {
+            Board(it.toLong(),"title$it","content$it", BoardStatus.ACTIVE, Instant.now(), null, null)
+        }
         `when` (boardReadUseCase.getBoard(anyLong())).thenAnswer { v ->
             val boardId = v.arguments[0] as Long
             boardList.associateBy { it.id }[boardId]
-                ?: throw IllegalArgumentException(" 조회할 수 없는 게시판입니다 id=$boardId")
+                ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid board ID: $boardId")
         }
         `when` (boardDtoMapper.toDtoDetail(any(Board::class.java))).thenAnswer { v ->
             val board = v.getArgument(0) as Board
@@ -84,7 +71,15 @@ class BoardQueryControllerTest(
                 .updatedAt(board.updatedAt)
                 .build()
         }
-        `when` (boardReadUseCase.findGeneralBy(pageable)).thenReturn(boardPage)
+        `when` (boardReadUseCase.findGeneralBy(any<PageRequest>())).thenAnswer { v ->
+            val pageable = v.getArgument<PageRequest>(0)
+            val pageContent = boardList.drop(pageable.pageNumber * pageable.pageSize)
+                .take(pageable.pageSize)
+                .takeIf { it.isNotEmpty() }
+                ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid page size: ${pageable.pageSize}")
+
+            PageImpl(pageContent, pageable, boardList.size.toLong())
+        }
         `when` (boardDtoMapper.toDtoSummary(any(Board::class.java))).thenAnswer { v ->
             val board = v.getArgument(0) as Board
             BoardSummaryResponse.builder()
@@ -96,40 +91,62 @@ class BoardQueryControllerTest(
         }
     }
 
+    "게시판 상세 조회" - {
 
-    "게시판 상세 조회"-{
-        "boardId로 게시판이 조회될때"{
-
-            val result = mvc.get("/api/v1/board/2"){
+        val mvcGet = fun(boardId: Long): ResultActionsDsl{
+            return mvc.get("/api/v1/board/$boardId") {
                 contentType = MediaType.APPLICATION_JSON
-            }.andExpect {
-                status { isOk() }
-            }.andReturn()
-
-            val response = objectMapper.readValue(result.response.contentAsString, BoardDetailResponse::class.java)
-
-            println(objectMapper.writeValueAsString(response))
-            println("response: $response")
+            }
         }
+
+        "정상 요청으로 인한 2xx 상태 반환" {
+            val mvcResult = mvcGet(1)
+                .andExpect { status { is2xxSuccessful() } }
+                .andReturn()
+
+            mvcResult.response.contentAsString
+                .let { objectMapper.readValue(it, BoardDetailResponse::class.java) }
+                .let { objectMapper.writeValueAsString(it) }
+                .let { println(it) }
+
+        }
+
+        "비정상 요청으로 인한 4xx 상태 반환" {
+            val mvcResult =mvcGet(200)
+                .andExpect { status{is4xxClientError()}}
+                .andReturn()
+
+            println("status: ${mvcResult.response.status}")
+        }
+
     }
 
-    "게시판 목록 조회"{
+    "게시판 목록 조회" - {
 
-        val result = mvc.get("/api/v1/board"){
-            param("page", "0")
-            param("size", "10")
-            contentType = MediaType.APPLICATION_JSON
-        }.andExpect {
-            status { isOk() }
-        }.andReturn()
+        val mvcGet = fun(page: Int): ResultActionsDsl{
+            return mvc.get("/api/v1/board") {
+                queryParam("page", page.toString())
+                queryParam("size", "10")
+                contentType = MediaType.APPLICATION_JSON
+            }
+        }
 
-        val jsonNode = objectMapper.readTree(result.response.contentAsString)
-        val contentJson = jsonNode["content"]
+        "정상 요청으로 인한 2xx 상태 반환" {
+            val mvcResult = mvcGet(1)
+                .andExpect { status { is2xxSuccessful() } }
+                .andReturn()
 
-        println("jsonNode :")
-        println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode))
+            mvcResult.response.contentAsString
+            .let { objectMapper.readTree(it) as ObjectNode }
+            .let { println(it) }
+        }
 
-        println("contentJson :")
-        println(objectMapper.writeValueAsString(contentJson))
+        "정상 요청으로 인한 4xx 상태 반환" {
+            val mvcResult = mvcGet(2)
+                .andExpect { status { is4xxClientError() } }
+                .andReturn()
+
+            println("status: ${mvcResult.response.status}")
+        }
     }
 })
