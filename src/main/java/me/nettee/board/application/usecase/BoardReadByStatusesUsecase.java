19f9eeb11121fba8c5d17296cd240ab0f6d3e6f8
silberbullet <pkw961028@naver.com>
2025-01-14 23:21:20 +0900

    chore(board): rename BoardReadByStatusesUsecase

--- src/main/java/me/nettee/board/application/usecase/BoardReadByStatusesUsecase.java
+++ src/main/java/me/nettee/board/application/usecase/BoardReadByStatusesUsecase.java
@@ -1,12 +1,11 @@
 package me.nettee.board.application.usecase;
 
-import java.util.List;
 import me.nettee.board.application.domain.Board;
 import me.nettee.board.application.domain.type.BoardStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import java.util.List;
 
 public interface BoardReadByStatusesUsecase {
     Page<Board> findByStatuses(Pageable pageable, List<BoardStatus> statuses);
 }
-
