a44c53906ce29f7ecb3da8301288654bee2f7730
silberbullet <pkw961028@naver.com>
2025-01-15 08:32:14 +0900

    test(board): refactor command controller test

--- src/test/kotlin/me/nettee/board/adapter/driving/web/BoardCommandControllerTest.kt
+++ src/test/kotlin/me/nettee/board/adapter/driving/web/BoardCommandControllerTest.kt
@@ -17,6 +17,7 @@ import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -27,130 +28,151 @@ import org.springframework.test.web.servlet.*
 @AutoConfigureMockMvc
 class BoardCommandControllerTest(
     @Autowired private val mvc: MockMvc,
+    @Autowired private val objectMapper: ObjectMapper,
     @MockitoBean private val boardCreateUseCase : BoardCreateUseCase,
     @MockitoBean private val boardUpdateUseCase : BoardUpdateUseCase,
     @MockitoBean private val boardDeleteUseCase : BoardDeleteUseCase,
     @MockitoBean private val boardDtoMapper : BoardDtoMapper
 ): FreeSpec({
 
-    "게시판 커맨트 컨트롤러 테스트"-{
+    // given
+    val boardDomain = Board.builder().id(1L).title("테스트게시판").content("테스트게시판내용").status(BoardStatus.ACTIVE).build()
+    val mvcRequest: (HttpMethod, String, Map<String, Any>, Any?) -> ResultActionsDsl =
+        { method, url, pathVariables, requestBody ->
+            mvc.request(method, url, *pathVariables.values.toTypedArray()) {
+                contentType = MediaType.APPLICATION_JSON
+                if (requestBody != null) {
+                    content = objectMapper.writeValueAsString(requestBody)
+                }
+            }
+        }
+
+    "[POST] 게시판 생성 요청" - {
         // given
-        val objectMapper = ObjectMapper()
-        val boardDomain = Board.builder()
-            .id(1L).title("테스트게시판").content("테스트게시판내용").status(BoardStatus.ACTIVE).build()
-        // when
-        `when` (boardDtoMapper.toDomain(any())).thenReturn(boardDomain)
+        val boardCreateResponse = boardDomain
 
-        "게시판 생성"-{
-            // given
-            val boardCreateResponse = boardDomain
-            val mvcPost = fun(request: BoardCreateCommand): ResultActionsDsl {
-                return mvc.post("/api/v1/board"){
-                    contentType = MediaType.APPLICATION_JSON
-                    content = objectMapper.writeValueAsString(request)
-                }.andDo { print() }
-            }
+        "[정상 요청] 제목과 내용이 3글자 이상과 공백이 아닐 때" - {
             // when
-            `when` (boardCreateUseCase.createBoard(any())).thenReturn(boardCreateResponse)
+            val createCommand = BoardCreateCommand("테스트게시판", "테스트게시판내용")
 
-            "필수값 NotNull 조건 충족으로 2XX 상태 반환"{
-                // given
-                val createCommand = BoardCreateCommand("테스트게시판","테스트게시판내용")
+            "2xx 응답 상태 반환" {
                 // then
-                mvcPost(createCommand)
-                    .andExpect{
-                        status { is2xxSuccessful() }
-                        jsonPath("board.id"){value(boardCreateResponse.id)}
-                        jsonPath("board.title"){value(boardCreateResponse.title)}
-                        jsonPath("board.content"){value(boardCreateResponse.content)}
-                    }
-            }
-            "필수값 NotNul 조건 불충족으로 4xx 상태 반환"-{
-                "title Null 4XX 상태 반환"{
-                    // given
-                    val failCreateCommand = BoardCreateCommand(null,"테스트게시판내용")
-                    // then
-                    mvcPost(failCreateCommand)
-                        .andExpect {
-                            status { is4xxClientError() }
-                        }
-                }
-                "content Null 4XX 상태 반환"{
-                    // given
-                    val failCreateCommand = BoardCreateCommand("테스트게시판",null)
-                    // then
-                    mvcPost(failCreateCommand)
-                        .andExpect {
-                            status { is4xxClientError() }
-                        }
-                }
-            }
-        }
-        "게시판 수정"-{
-            // given
-            val boardUpdateResponse = Board.builder()
-                .id(1L).title("테스트게시판수정").content("테스트게시판내용수정").status(BoardStatus.ACTIVE).build()
-
-            val mvcPut = fun(request: BoardUpdateCommand): ResultActionsDsl {
-                return mvc.patch("/api/v1/board/{id}", boardDomain.id){
-                    contentType = MediaType.APPLICATION_JSON
-                    content = objectMapper.writeValueAsString(request)
-                }.andDo { print() }
-            }
-            // when
-            `when` (boardUpdateUseCase.updateBoard(any())).thenReturn(boardUpdateResponse)
-
-            "필수값 NotNull 조건 충족으로 2XX 상태 반환"{
-                // given
-                val updateCommand = BoardUpdateCommand("테스트게시판수정","테스트게시판내용수정")
-                // then
-                mvcPut(updateCommand)
-                    .andExpect{
-                        status { is2xxSuccessful() }
-                        jsonPath("board.id"){value(boardDomain.id)}
-                        jsonPath("board.title"){value(boardUpdateResponse.title)}
-                        jsonPath("board.content"){value(boardUpdateResponse.content)}
-                    }
-            }
-            "필수값 NotNul 조건 불충족으로 4xx 상태 반환"-{
-                "title Null 4XX 상태 반환" {
-                    // given
-                    val failUpdateCommand = BoardUpdateCommand(null, "테스트게시판내용수정")
-                    // then
-                    mvcPut(failUpdateCommand)
-                        .andExpect {
-                            status { is4xxClientError() }
-                        }
-                }
-                "content Null 4XX 상태 반환" {
-                    // given
-                    val failUpdateCommand = BoardUpdateCommand("테스트게시판수정", null)
-                    // then
-                    mvcPut(failUpdateCommand)
-                        .andExpect {
-                            status { is4xxClientError() }
-                        }
-                }
-            }
-        }
-        "게시판 삭제"-{
-            // given
-            val mvcDelete = fun(): ResultActionsDsl {
-                return mvc.delete("/api/v1/board/{id}", boardDomain.id){
-                    contentType = MediaType.APPLICATION_JSON
-                }.andDo { print() }
-            }
-
-            "삭제 성공으로 2xx 상태 반환"{
-                // when
-                doNothing().`when`(boardDeleteUseCase).deleteBoard(any())
-
-                // then
-                mvcDelete()
+                mvcRequest(HttpMethod.POST, "/api/v1/board", emptyMap(), createCommand)
                     .andExpect {
                         status { is2xxSuccessful() }
+                        jsonPath("board.id") { value(boardCreateResponse.id) }
+                        jsonPath("board.title") { value(boardCreateResponse.title) }
+                        jsonPath("board.content") { value(boardCreateResponse.content) }
                     }
+                    .andDo { print() }
+                    .andReturn()
             }
         }
+
+        "[실패 요청] 제목과 내용이 3글자 미만 일 때" - {
+            // when
+            val failCreateCommand = BoardCreateCommand("테스", "테스")
+
+            // then
+            "4xx 응답 상태 반환" {
+                mvcRequest(HttpMethod.POST, "/api/v1/board", emptyMap(), failCreateCommand)
+                    .andExpect {
+                        status { is4xxClientError() }
+                    }
+                    .andDo { print() }
+                    .andReturn()
+            }
+        }
+
+        "[실패 요청] 제목과 내용이 공백 일 때" - {
+            // when
+            val failCreateCommand = BoardCreateCommand(null, null)
+
+            // then
+            "4xx 응답 상태 반환" {
+                mvcRequest(HttpMethod.POST, "/api/v1/board", emptyMap(), failCreateCommand)
+                    .andExpect {
+                        status { is4xxClientError() }
+                    }
+                    .andDo { print() }
+                    .andReturn()
+            }
+        }
+    }
+
+    "[PATCH] 게시판 수정 요청" - {
+        // given
+        val boardUpdateResponse = Board.builder()
+            .id(1L).title("테스트게시판수정").content("테스트게시판내용수정").status(BoardStatus.ACTIVE).build()
+
+        "[정상 요청] 제목과 내용이 3글자 이상과 공백이 아닐 때" - {
+            // when
+            val updateCommand = BoardUpdateCommand("테스트게시판", "테스트게시판내용")
+
+            "2xx 정상 상태 반환" {
+                // then
+                mvcRequest(HttpMethod.PATCH, "/api/v1/board/{id}", mapOf("id" to boardUpdateResponse.id), updateCommand)
+                    .andExpect {
+                        status { is2xxSuccessful() }
+                        jsonPath("board.id") { value(boardUpdateResponse.id) }
+                        jsonPath("board.title") { value(updateCommand.title) }
+                        jsonPath("board.content") { value(updateCommand.content) }
+                    }
+                    .andDo { print() }
+                    .andReturn()
+            }
+        }
+
+        "[실패 요청] 제목과 내용이 3글자 이하 일 때" - {
+            // when
+            val failUpdateCommand = BoardUpdateCommand("테스", "테스")
+
+            "4xx 응답 상태 반환" {
+                // then
+                mvcRequest(HttpMethod.PATCH, "/api/v1/board/{id}", mapOf("id" to boardUpdateResponse.id), failUpdateCommand)
+                    .andExpect {
+                        status { is4xxClientError() }
+                    }
+                    .andDo { print() }
+                    .andReturn()
+            }
+        }
+
+        "[실패 요청] 제목과 내용이 공백 일 때" - {
+            // when
+            val failUpdateCommand = BoardUpdateCommand(null, null)
+
+            "4xx 응답 상태 반환" {
+                // then
+                mvcRequest(HttpMethod.PATCH, "/api/v1/board/{id}", mapOf("id" to boardUpdateResponse.id), failUpdateCommand)
+                    .andExpect {
+                        status { is4xxClientError() }
+                    }
+                    .andDo { print() }
+                    .andReturn()
+            }
+        }
+    }
+
+    "[DELETE] 게시판 삭제 요청" - {
+        "[정상 요청] 해당 커뮤니티 게시판 ID가 존재 할 때" - {
+            "2xx 응답 상태 반환" {
+                mvcRequest(HttpMethod.DELETE, "/api/v1/board/{id}", mapOf("id" to boardDomain.id), null)
+                .andExpect {
+                    status { is2xxSuccessful() }
+                }
+            }
+        }
+
+        "[실패 요청] 해당 커뮤니티 게시판 ID 미존재 할 때" - {
+
+        }
+    }
+
+    beforeSpec {
+        `when` (boardDtoMapper.toDomain(any())).thenReturn(boardDomain)
+        `when` (boardCreateUseCase.createBoard(any())).thenReturn(boardDomain)
+        `when` (boardUpdateUseCase.updateBoard(any())).thenReturn(boardDomain)
+        doNothing().`when`(boardDeleteUseCase).deleteBoard(any())
     }
 })
