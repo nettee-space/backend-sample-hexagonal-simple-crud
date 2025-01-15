5c0575524229df5f6c0a61b437116b480060e124
taewookimm <kimtaewoo8487@gmail.com>
2025-01-15 17:18:01 +0900

    feat(boardEntity): modified boardEntity

--- src/main/java/me/nettee/board/adapter/driven/persistence/entity/BoardEntity.java
+++ src/main/java/me/nettee/board/adapter/driven/persistence/entity/BoardEntity.java
@@ -1,56 +1,45 @@
 package me.nettee.board.adapter.driven.persistence.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import me.nettee.board.application.domain.type.BoardStatus;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.Instant;
 import java.util.Objects;
 
 @Getter
 @DynamicUpdate
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Entity(name = "board")
 public class BoardEntity {
-
     @Id
-    @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "title", nullable = false)
     private String title;
 
-    @Lob
-    @Column(name = "content", columnDefinition = "LONGTEXT", nullable = false)
     private String content;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false)
     private BoardStatus status;
 
     @CreatedDate
-    @Column(name = "created_at")
     private Instant createdAt;
 
     @LastModifiedDate
-    @Column(name = "updated_at")
     private Instant updatedAt;
-
-    @Column(name = "deleted_at")
-    private Instant deletedAt;
 
     @Builder
     public BoardEntity(String title, String content, BoardStatus status) {
         this.title = title;
         this.content = content;
         this.status = status;
-        this.deletedAt = null;
     }
 
     @Builder(
@@ -58,7 +47,8 @@ public class BoardEntity {
             builderMethodName = "prepareUpdate",
             buildMethodName = "update"
     )
-    public void updateBoard(String title, String content, BoardStatus status, Instant updatedAt) {
+    public void updateBoard(String title, String content, BoardStatus status) {
+        // DB단계가 아닌 Application단계에서도 null 체크
         Objects.requireNonNull(title, "title cannot be null");
         Objects.requireNonNull(content, "content cannot be null");
         Objects.requireNonNull(status, "status cannot be null");
@@ -66,15 +56,11 @@ public class BoardEntity {
         this.title = title;
         this.content = content;
         this.status = status;
-        this.updatedAt = updatedAt;
     }
 
+    // delete의 경우 soft delete로 처리
+    // 삭제 시 deletedAt을 사용 안하고 updatedAt으로 관리
     public void softDelete() {
         this.status = BoardStatus.REMOVED;
-    }
-
-    // TODO: 임시용 코드 개선 후 꼭 삭제
-    public void settingCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
     }
 }
