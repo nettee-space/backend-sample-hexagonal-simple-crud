aa0349b2c681601f0949d940a273f353934ab4a1
taewookimm <kimtaewoo8487@gmail.com>
2025-01-24 22:09:48 +0900

    feat(jpa): add JpaConfig, BaseEntity, BaseTimeEntity

--- src/main/java/me/nettee/board/adapter/driven/persistence/entity/BoardEntity.java
+++ src/main/java/me/nettee/board/adapter/driven/persistence/entity/BoardEntity.java
@@ -3,13 +3,10 @@ package me.nettee.board.adapter.driven.persistence.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import me.nettee.board.application.domain.type.BoardStatus;
+import me.nettee.core.jpa.entity.BaseTimeEntity;
 import org.hibernate.annotations.DynamicUpdate;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.time.Instant;
 import java.util.Objects;
 
 @Getter
@@ -17,10 +14,7 @@ import java.util.Objects;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 @Entity(name = "board")
-public class BoardEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class BoardEntity extends BaseTimeEntity {
 
     private String title;
 
@@ -29,11 +23,6 @@ public class BoardEntity {
     @Enumerated(EnumType.STRING)
     private BoardStatus status;
 
-    @CreatedDate
-    private Instant createdAt;
-
-    @LastModifiedDate
-    private Instant updatedAt;
 
     @Builder
     public BoardEntity(String title, String content, BoardStatus status) {
@@ -48,7 +37,6 @@ public class BoardEntity {
             buildMethodName = "update"
     )
     public void updateBoard(String title, String content, BoardStatus status) {
-        // DB단계가 아닌 Application단계에서도 null 체크
         Objects.requireNonNull(title, "title cannot be null");
         Objects.requireNonNull(content, "content cannot be null");
         Objects.requireNonNull(status, "status cannot be null");
@@ -58,8 +46,6 @@ public class BoardEntity {
         this.status = status;
     }
 
-    // delete의 경우 soft delete로 처리
-    // 삭제 시 deletedAt을 사용 안하고 updatedAt으로 관리
     public void softDelete() {
         this.status = BoardStatus.REMOVED;
     }
