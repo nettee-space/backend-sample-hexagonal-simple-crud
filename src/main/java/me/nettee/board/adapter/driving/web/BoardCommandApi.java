9356e69f2c61678d992a67b0f37514c01b92485e
jingyu <wlsrb97@naver.com>
2025-01-27 02:22:42 +0900

    refactor: Rename controller to API

--- src/main/java/me/nettee/board/adapter/driving/web/BoardCommandApi.java
+++ src/main/java/me/nettee/board/adapter/driving/web/BoardCommandApi.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/board")
 @RequiredArgsConstructor
-public class BoardCommandController {
+public class BoardCommandApi {
 
     private final BoardCreateUseCase boardCreateUseCase;
     private final BoardUpdateUseCase boardUpdateUseCase;
