516585357ca585a12f9ece829fa70d290c97da23
JeongJeongYong <wjddyd94@naver.com>
2025-02-05 23:24:31 +0900

    test(board): refactor `BoardQueryAdapterTest`

--- src/test/kotlin/me/nettee/board/adapter/driven/persistence/BoardQueryAdapterTest.kt
+++ src/test/kotlin/me/nettee/board/adapter/driven/persistence/BoardQueryAdapterTest.kt
@@ -1,8 +1,6 @@
 package me.nettee.board.adapter.driven.persistence
 
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.matchers.collections.shouldBeIn
-import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import jakarta.persistence.EntityManager
 import me.nettee.board.adapter.driven.mapper.BoardEntityMapper
@@ -14,18 +12,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import java.time.Instant
 
 @ComponentScan(basePackageClasses = [BoardEntityMapper::class])
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@EnableJpaAuditing
-class BoardQueryAdapterSpringBootTest (
-        @Autowired private val boardJpaRepository : BoardJpaRepository,
-        @Autowired private val boardEntityMapper : BoardEntityMapper,
-        @Autowired private val entityManager : EntityManager,
-) : FreeSpec({
+class BoardQueryAdapterSpringBootTest(
+    @Autowired private val boardJpaRepository : BoardJpaRepository,
+    @Autowired private val boardEntityMapper : BoardEntityMapper,
+    @Autowired private val entityManager : EntityManager
+): FreeSpec({
     val boardQueryAdapter = BoardQueryAdapter(boardEntityMapper)
 
     boardQueryAdapter.setEntityManager(entityManager)
@@ -34,8 +29,6 @@ class BoardQueryAdapterSpringBootTest (
             .title("제목")
             .content("내용")
             .status(BoardStatus.ACTIVE)
-            .createdAt(Instant.now())
-            .updatedAt(Instant.now())
             .build()
 
     beforeSpec {
@@ -66,19 +59,20 @@ class BoardQueryAdapterSpringBootTest (
 
     "[Read] 게시글 목록 조회" - { // RootNode
         // Given: 여러 게시물들을 저장
-        val boardEnties =
-                (1..5).flatMap {
-                listOf(
-                        BoardEntity.builder()
-                                .title("title$it")
-                                .content("content$it")
-                                .status(BoardStatus.ACTIVE)
-                                .build()
-                    )
-                }
+        val boardEnties = (1..5).flatMap {
+            listOf(
+                BoardEntity.builder()
+                    .title("title$it")
+                    .content("content$it")
+                    .status(BoardStatus.ACTIVE)
+                    .build()
+            )
+        }
+
         boardJpaRepository.saveAll(
             boardEnties
         )
+
         "[정상] 게시글이 존재할 때" - {
             // When: 게시글 목록 조회
             val pageable: Pageable = PageRequest.of(0, 10)
@@ -97,23 +91,16 @@ class BoardQueryAdapterSpringBootTest (
 
     "[Read] 특정 상태 목록으로 게시글 목록을 조회" - {
         // Given: 특정 상태에 해당하는 게시글 저장
-        val boardEntities = setOf(
+        val boardEntities = ((1..3).flatMap {
+            listOf(
                 BoardEntity.builder()
-                        .title("게시글 1")
-                        .content("내용 1")
-                        .status(BoardStatus.ACTIVE)
-                        .build(),
-                BoardEntity.builder()
-                        .title("게시글 2")
-                        .content("내용 2")
-                        .status(BoardStatus.PENDING)
-                        .build(),
-                BoardEntity.builder()
-                        .title("게시글 3")
-                        .content("내용 3")
-                        .status(BoardStatus.ACTIVE)
-                        .build()
-        )
+                    .title("게시글 $it")
+                    .content("내용 $it")
+                    .status(if (it % 2 == 0) { BoardStatus.ACTIVE } else { BoardStatus.PENDING })
+                    .build()
+            )
+        })
+
         boardJpaRepository.saveAll(
             boardEntities
         )
@@ -127,8 +114,5 @@ class BoardQueryAdapterSpringBootTest (
         "[검증1] 필터링된 게시글 총 개수를 검증" {
             page.totalElements shouldBe expectedSize
         }
-
-
     }
-
 })
