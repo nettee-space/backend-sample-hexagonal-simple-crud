0ea5765bb93a92e788e588d169b128c0fd6340b8
jingyu <wlsrb97@naver.com>
2025-02-13 21:11:03 +0900

    test(board): clean up test code - BoardCommandControllerTest: remove wildcard imports - BoardQueryControllerTest: remove unnecessary blank lines

--- src/test/kotlin/me/nettee/board/adapter/driving/web/BoardCommandApiTest.kt
+++ src/test/kotlin/me/nettee/board/adapter/driving/web/BoardCommandApiTest.kt
@@ -21,7 +21,9 @@ import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.web.servlet.*
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActionsDsl
+import org.springframework.test.web.servlet.request
 
 @ExtendWith(SpringExtension::class)
 @WebMvcTest(BoardCommandApi::class)
