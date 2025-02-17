bfdc4e56d9c1f53a69d7e39eb40dea5a3f060256
JeongJeongYong <wjddyd94@naver.com>
2025-02-18 00:18:20 +0900

    refactor(board): delete invalid annotations and method

--- src/main/java/me/nettee/board/adapter/driven/persistence/mapper/BoardEntityMapper.java
+++ src/main/java/me/nettee/board/adapter/driven/persistence/mapper/BoardEntityMapper.java
@@ -2,28 +2,20 @@ package me.nettee.board.adapter.driven.persistence.mapper;
 
 import java.util.Optional;
 import me.nettee.board.adapter.driven.persistence.entity.BoardEntity;
-import me.nettee.board.adapter.driven.persistence.entity.type.BoardEntityStatus;
 import me.nettee.board.application.domain.Board;
-import me.nettee.board.application.domain.type.BoardStatus;
 import me.nettee.board.application.model.BoardQueryModels.BoardDetail;
 import me.nettee.board.application.model.BoardQueryModels.BoardSummary;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface BoardEntityMapper {
 
-    @Mapping(source="status", target="status", qualifiedByName="toBoardStatus")
     Board toDomain(BoardEntity boardEntity);
 
-    @Mapping(source="status", target="status", qualifiedByName="toBoardEntityStatus")
     BoardEntity toEntity(Board board);
 
-    @Mapping(source="status", target="status", qualifiedByName="toBoardStatus")
     BoardDetail toBoardDetail(BoardEntity boardEntity);
 
-    @Mapping(source="status", target="status", qualifiedByName="toBoardStatus")
     BoardSummary toBoardSummary(BoardEntity boardEntity);
 
     default Optional<Board> toOptionalDomain(BoardEntity boardEntity) {
@@ -34,23 +26,4 @@ public interface BoardEntityMapper {
         return Optional.ofNullable(toBoardDetail(boardEntity));
     }
 
-    @Named("toBoardStatus")
-    static BoardStatus toBoardStatus(BoardEntityStatus status) {
-        return switch (status) {
-            case REMOVED -> BoardStatus.REMOVED;
-            case PENDING -> BoardStatus.PENDING;
-            case ACTIVE -> BoardStatus.ACTIVE;
-            case SUSPENDED -> BoardStatus.SUSPENDED;
-        };
-    }
-
-    @Named("toBoardEntityStatus")
-    static BoardEntityStatus toBoardEntityStatus(BoardStatus status) {
-        return switch (status) {
-            case REMOVED -> BoardEntityStatus.REMOVED;
-            case PENDING -> BoardEntityStatus.PENDING;
-            case ACTIVE -> BoardEntityStatus.ACTIVE;
-            case SUSPENDED -> BoardEntityStatus.SUSPENDED;
-        };
-    }
 }
