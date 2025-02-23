645385a8dee9aaa20bd38028221480624a484983
silberbullet <pkw961028@naver.com>
2025-02-24 00:10:12 +0900

    chore(board): fix merge fault

--- src/main/java/me/nettee/board/application/service/BoardQueryService.java
+++ src/main/java/me/nettee/board/application/service/BoardQueryService.java
@@ -3,14 +3,16 @@ package me.nettee.board.application.service;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import me.nettee.board.application.domain.type.BoardStatus;
-import me.nettee.board.application.model.BoardQueryModel.BoardDetail;
-import me.nettee.board.application.model.BoardQueryModel.BoardSummary;
+import me.nettee.board.application.model.BoardQueryModels.BoardDetail;
+import me.nettee.board.application.model.BoardQueryModels.BoardSummary;
 import me.nettee.board.application.port.BoardQueryPort;
 import me.nettee.board.application.usecase.BoardReadByStatusesUseCase;
 import me.nettee.board.application.usecase.BoardReadUseCase;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import static me.nettee.board.application.exception.BoardQueryErrorCode.BOARD_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -20,8 +22,8 @@ public class BoardQueryService implements BoardReadUseCase, BoardReadByStatusesU
 
     @Override
     public BoardDetail getBoard(Long id) {
-        return boardQueryPort.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
+        return boardQueryPort.findById(id)
+                .orElseThrow(BOARD_NOT_FOUND::exception);
     }
 
     @Override
