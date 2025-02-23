44e29462eea52c6bc0591071b3a4b3d6b99b5ab9
taewookimm <kimtaewoo8487@gmail.com>
2025-02-23 14:46:20 +0900

    refactor(board): add mock obj initialization

--- src/test/kotlin/me/nettee/board/application/service/BoardCommandServiceTest.kt
+++ src/test/kotlin/me/nettee/board/application/service/BoardCommandServiceTest.kt
@@ -3,6 +3,7 @@ package me.nettee.board.application.service
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -12,6 +13,7 @@ import me.nettee.board.application.exception.BoardCommandException
 import me.nettee.board.application.port.BoardCommandPort
 import java.util.Optional
 import me.nettee.board.application.exception.BoardCommandErrorCode.BOARD_NOT_FOUND
+import me.nettee.board.application.exception.BoardCommandErrorCode.DEFAULT
 
 
 class BoardCommandServiceTest : FreeSpec({
@@ -19,67 +21,59 @@ class BoardCommandServiceTest : FreeSpec({
     val boardCommandPort = mockk<BoardCommandPort>()
     val boardCommandService = BoardCommandService(boardCommandPort)
 
+    beforeTest {
+        clearMocks(boardCommandPort, answers = true, recordedCalls = true)
+    }
+
     "BoardCommandService" - {
         "create" {
             // given
             val board = Board()
-            every {
-                boardCommandPort.create(board)
-            } returns board
+            every { boardCommandPort.create(board) } returns board
 
             // when
             val result = boardCommandService.createBoard(board)
 
             // then
             result shouldBe board
-            verify { boardCommandPort.create(board) }
+            verify(exactly = 1) { boardCommandPort.create(board) }
         }
 
         "update" {
             // given
             val board = Board()
-            every {
-                boardCommandPort.update(board)
-            } returns board
+            every { boardCommandPort.update(board) } returns board
 
             // when
             val result = boardCommandService.updateBoard(board)
 
             // then
             result shouldBe board
-            verify { boardCommandPort.update(board) }
+            verify(exactly = 1) { boardCommandPort.update(board) }
         }
 
-        // TO-DO 테스트 코드 수정이 필요합니다.
         "delete" - {
-
-            "Soft Delete - 게시판을 삭제" {
-                // Mock 생성
-                val boardCommandPort = mockk<BoardCommandPort>()
-                val boardCommandService = BoardCommandService(boardCommandPort)
-
+            "Soft Delete - 정상적으로 삭제된 경우" {
                 // given
                 val boardId = 1L
                 val board = Board()
 
-                every { boardCommandPort.findById(boardId) } returns Optional.of(board)
                 every { boardCommandPort.updateStatus(boardId, REMOVED) } returns Unit
+                every { boardCommandPort.findById(boardId) } returns Optional.of(board)
 
                 // when
                 boardCommandService.deleteBoard(boardId)
 
                 // then
-                verify(exactly = 1) { boardCommandPort.findById(boardId) }
                 verify(exactly = 1) { boardCommandPort.updateStatus(boardId, REMOVED) }
+                verify(exactly = 1) { boardCommandPort.findById(boardId) }
             }
 
-            "게시판을 찾지 못하면 예외를 발생" {
-                // Mock 생성
-                val boardCommandPort = mockk<BoardCommandPort>()
-                val boardCommandService = BoardCommandService(boardCommandPort)
-
+            "Exception - 게시판을 찾지 못하면 예외 처리" {
                 // given
-                val boardId = 999L
+                val boardId = 1L
+
+                every { boardCommandPort.updateStatus(boardId, REMOVED) } returns Unit
                 every { boardCommandPort.findById(boardId) } returns Optional.empty()
 
                 // when & then
@@ -88,9 +82,26 @@ class BoardCommandServiceTest : FreeSpec({
                 }
                 exception.errorCode shouldBe BOARD_NOT_FOUND
 
-                // updateStatus가 호출되면 안 된다
+                verify(exactly = 1) { boardCommandPort.updateStatus(boardId, REMOVED) }
                 verify(exactly = 1) { boardCommandPort.findById(boardId) }
-                verify(exactly = 0) { boardCommandPort.updateStatus(any(), any()) }
+            }
+
+            "Exception - 삭제 후 조회된 Board의 상태가 REMOVED가 아니면 예외 처리" {
+                // given
+                val boardId = 1L
+                val board = Board()
+
+                every { boardCommandPort.updateStatus(boardId, REMOVED) } returns Unit
+                every { boardCommandPort.findById(boardId) } returns Optional.of(board)
+
+                // when & then
+                val exception = shouldThrow<BoardCommandException> {
+                    boardCommandService.deleteBoard(boardId)
+                }
+                exception.errorCode shouldBe DEFAULT
+
+                verify(exactly = 1) { boardCommandPort.updateStatus(boardId, REMOVED) }
+                verify(exactly = 1) { boardCommandPort.findById(boardId) }
             }
         }
     }
