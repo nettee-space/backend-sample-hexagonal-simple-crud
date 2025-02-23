bd151381cb4a045873db1007a15f3573ce4315a4
silberbullet <pkw961028@naver.com>
2025-02-24 00:16:59 +0900

    chore(board): fix merge fault

--- src/main/java/me/nettee/board/adapter/driven/persistence/entity/type/BoardEntityStatus.java
+++ src/main/java/me/nettee/board/adapter/driven/persistence/entity/type/BoardEntityStatus.java
@@ -1,6 +1,9 @@
 package me.nettee.board.adapter.driven.persistence.entity.type;
+
 import me.nettee.board.application.domain.type.BoardStatus;
+
 import static me.nettee.board.application.exception.BoardCommandErrorCode.DEFAULT;
+
 public enum BoardEntityStatus {
     REMOVED(0),
     PENDING(10),
@@ -32,7 +35,7 @@ public enum BoardEntityStatus {
             case 10 -> PENDING;
             case 20 -> ACTIVE;
             case 30 -> SUSPENDED;
-            default -> throw DEFAULT.defaultException();
+            default -> throw DEFAULT.exception();
         };
     }
 }
